2c7895cf41aab529d43a9eabe8ef96dce69b9b7d
jackbukhsa <jack.biggs@ukhsa.gov.uk>
2024-09-03 13:55:33 +0100

    correct the files in here so that they will pass black, flake8 and pylint

--- python_test_2.py
+++ python_test_2.py
@@ -1,20 +1,40 @@
-import os, sys
+"""
+This module contains an example function to demonstrate
+the usage of docstrings and Pylint error handling.
+"""
+
+import os
+
+print(os.getcwd())
 
 
 def example_function(param1, param2):
-    # This function has several issues
+    """
+    This function compares two prints to check if they are equal.
+
+    Args:
+        print1 (type): Description of print1.
+        print2 (type): Description of print2.
+
+    Returns:
+        bool: True if the prints are equal, False otherwise.
+    """
+
     if param1 == param2:
         print("Parameters are equal")
         return True
-    else:
-        print("Parameters are not equal")
-        return False
+
+    print("Parameters are not equal")
+    return False
 
 
 def main():
+    """
+    This is the main function that will call the example function
+    """
+
     example_function("hello", "world")
     print("This is a test")
-    import datetime  # unnecessary import
 
     a = 1
     b = 2
